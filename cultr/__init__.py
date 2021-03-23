19cf58ffb67970070b8703e33735c83925766fb3
TrixiS <oficialmorozov@gmail.com>
2021-03-23 17:06:28 +0500

    Fill init package data

---
+++ cultr/__init__.py
@@ -0,0 +1,8 @@
+
+__title__ = "cultr"
+__author__ = "TrixiS"
+__license__ = "MIT"
+__copyright__ = "Copyright 2021-present TrixiS"
+__version__ = "0.0.1"
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
